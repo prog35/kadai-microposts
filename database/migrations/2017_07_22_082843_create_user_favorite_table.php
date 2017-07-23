b47740c680f3b6d30528a30e02de6f24867903a1
taka8 <learning.prog35@gmail.com>
2017-07-23 01:43:36 +0000

    favorite bug fix 1

--- database/migrations/2017_07_22_082843_create_user_favorite_table.php
+++ database/migrations/2017_07_22_082843_create_user_favorite_table.php
@@ -18,8 +18,8 @@ class CreateUserFavoriteTable extends Migration
             $table->integer('micropost_id')->unsigned()->index();
             $table->timestamps();
             
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('micropost_id')->references('id')->on('microposts');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('micropost_id')->references('id')->on('microposts')->onDelete('cascade');
             
             $table->unique(['user_id', 'micropost_id']);
         });
